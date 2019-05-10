38ae197f2326565e42296832574fd1872b0f36e3
Peter Donald <peter@realityforge.org>
2019-05-10 16:30:13 +1000

    Add date scalar type

--- src/main/java/org/realityforge/graphql/domgen/Scalars.java
+++ src/main/java/org/realityforge/graphql/domgen/Scalars.java
@@ -5,6 +5,7 @@ import graphql.language.StringValue;
 import graphql.schema.Coercing;
 import graphql.schema.GraphQLScalarType;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -13,7 +14,17 @@ import java.util.Date;
 public final class Scalars
 {
   public static GraphQLScalarType DATE_TIME =
-    new GraphQLScalarType( "DateTime", "DataTime scalar", new DateTimeCoercing() );
+    GraphQLScalarType.newScalar()
+      .name( "DateTime" )
+      .description( "DataTime scalar" )
+      .coercing( new DateTimeCoercing() )
+      .build();
+  public static GraphQLScalarType DATE =
+    GraphQLScalarType.newScalar()
+      .name( "Date" )
+      .description( "Date scalar" )
+      .coercing( new DateCoercing() )
+      .build();
 
   private static class DateTimeCoercing
     implements Coercing<Date, String>
@@ -37,6 +48,43 @@ public final class Scalars
       if ( input instanceof StringValue )
       {
         return Date.from( ZonedDateTime.parse( ( (StringValue) input ).getValue() ).toInstant() );
+      }
+      else if ( input instanceof IntValue )
+      {
+        final long epochMilli = ( (IntValue) input ).getValue().longValue();
+        return Date.from( Instant.ofEpochMilli( epochMilli ) );
+      }
+      else
+      {
+        return null;
+      }
+    }
+  }
+
+  private static class DateCoercing
+    implements Coercing<Date, String>
+  {
+    @Override
+    public String serialize( final Object input )
+    {
+      final Date date = (Date) input;
+      return date.toInstant().atZone( ZoneId.systemDefault() ).format( DateTimeFormatter.ISO_LOCAL_DATE );
+    }
+
+    @Override
+    public Date parseValue( final Object input )
+    {
+      return parseLiteral( input );
+    }
+
+    @Override
+    public Date parseLiteral( final Object input )
+    {
+      if ( input instanceof StringValue )
+      {
+        return Date.from( LocalDate.parse( ( (StringValue) input ).getValue() )
+                            .atStartOfDay( ZoneId.systemDefault() )
+                            .toInstant() );
       }
       else if ( input instanceof IntValue )
       {
